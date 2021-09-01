e89ddde21cfde11dfcb18fa01e561911354f16cb
supriyamahesh <89924610+supriyamahesh@users.noreply.github.com>
2021-09-01 20:15:51 +0530

    Assignment1 commit

---
+++ Datatype.java
@@ -0,0 +1,20 @@
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		 int a=10;
+	      float b=20.67867f;
+	      double c=776;
+	      char d='S';
+	      String e="Java" ;
+
+	      System.out.println("int is "+a);
+	      System.out.println(b);
+	      System.out.println(c);
+	      System.out.println(d);
+	      System.out.println(e);
+	      
+
+	}
+
+}
